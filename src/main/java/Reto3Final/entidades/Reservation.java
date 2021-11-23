e083590250900ae48de26ba0d13907910f7553cd
= <jaaygi01@gmail.com>
2021-11-23 17:38:41 -0500

    Agregamos todo del Score para poder terminar el reto

--- src/main/java/Reto3Final/entidades/Reservation.java
+++ src/main/java/Reto3Final/entidades/Reservation.java
@@ -8,12 +8,14 @@ package Reto3Final.entidades;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -48,4 +50,8 @@ public class Reservation implements Serializable{
     @JoinColumn(name = "name")
     @JsonIgnoreProperties({"reservations", "messages"})
     private Client client;
+    
+    @OneToOne(cascade = {CascadeType.REMOVE}, mappedBy ="reservation")
+    @JsonIgnoreProperties("reservation")
+    private Score score;
 }
